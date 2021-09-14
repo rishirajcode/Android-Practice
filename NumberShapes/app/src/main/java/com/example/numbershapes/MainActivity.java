28729651072ac23f83904f9489995b1f3af0a6ce
Rishi Raj <1999rishiraj@gmail.com>
2021-09-15 02:25:13 +0530

    Ready to Push
    NumberShapes

---
+++ NumberShapes/app/src/main/java/com/example/numbershapes/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.numbershapes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    class Number {
+        int number ;
+
+        public boolean isSquare() {
+            double squareRoot = Math.sqrt(number);
+
+            if (squareRoot == Math.floor(squareRoot)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        public boolean isTriangular (){
+                int x = 1;
+
+                int triangularNumber = 1 ;
+
+                while (triangularNumber < number) {
+                    x++;
+                    triangularNumber = triangularNumber + x ;
+                }
+                if (triangularNumber == number){
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+
+    public void testNumber(View view) {
+        String msg ;
+
+
+        Log.i("Info", "Button pressed");
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+
+        if (editText.getText().toString().isEmpty()) {
+            msg = "Please enter a number";
+
+            Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+        } else {
+
+            Number myNumber = new Number();
+            myNumber.number = 49;
+
+            myNumber.number = Integer.parseInt(editText.getText().toString());
+            String message = editText.getText().toString();
+
+            if (myNumber.isSquare() && myNumber.isTriangular()) {
+                message += "is square and triangular";
+            } else if (myNumber.isSquare()) {
+                message += "is square but not triangular";
+            } else if (myNumber.isTriangular()) {
+                message += "is triangular but not square";
+            } else {
+                message += "is neither triangular nor square";
+            }
+
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
